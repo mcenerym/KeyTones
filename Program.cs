78e17e910b28079719cb2cff3168111610954ab8
mcenerym <mike.mcenery1@gmail.com>
2024-09-09 11:23:06 -0400

    Added tetris sounds and key const

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Media;
+using System.Runtime.InteropServices;
+
 namespace KeyTones
 {
 	internal static class Program
@@ -10,23 +13,79 @@ namespace KeyTones
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault( false );
+			Application.Run( new KeyTonesApplicationContext() );
+		}
+	}
 
-			using( NotifyIcon icon = new NotifyIcon() )
+	public class KeyTonesApplicationContext : ApplicationContext
+	{
+		private NotifyIcon TrayIcon { get; set; } = new();
+		private WMPLib.WindowsMediaPlayer wplayer = new();
+
+		public KeyTonesApplicationContext()
+        {
+			TrayIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon( Application.ExecutablePath );
+			TrayIcon.ContextMenuStrip = new ContextMenuStrip()
 			{
-				icon.Icon = System.Drawing.Icon.ExtractAssociatedIcon( Application.ExecutablePath );
-				icon.ContextMenuStrip = new ContextMenuStrip()
-				{
-					Items = { new ToolStripMenuItem( "Exit", null, Exit ) }
-				};
+				Items = {
+					new ToolStripMenuItem( "Exit", null, Exit )
+				}
+			};
 
-				icon.Visible = true;
-				Application.Run();
-			}
+			TrayIcon.Visible = true;
+
+			StartKeyListening();
 		}
 
-		static void Exit( object? sender, EventArgs e )
+		private void StartKeyListening()
+		{
+			var keyListeningThread = new Thread( new ThreadStart( ListenForKey ) );
+			keyListeningThread.Start();
+		}
+
+		private void PlaySound()
+		{
+			var soundsDirectory = $@"{AppDomain.CurrentDomain.BaseDirectory}\sounds";
+			wplayer.URL = $"{soundsDirectory}/blockturn.mp3";
+			wplayer.settings.volume = 5;
+			wplayer.controls.play();
+		}
+
+		private void Exit( object? sender, EventArgs e )
 		{
 			Application.Exit();
+		}
+
+		[DllImport( "User32.dll" )]
+		private static extern short GetAsyncKeyState( int vKey );
+
+		private static readonly int VK_SNAPSHOT = 0x2C; //This is the print-screen key.
+
+		private void ListenForKey()
+		{
+			while( true )
+			{
+				short keyState = GetAsyncKeyState( VK_SNAPSHOT );
+
+				//Check if the MSB is set. If so, then the key is pressed.
+				bool prntScrnIsPressed = ( ( keyState >> 15 ) & 0x0001 ) == 0x0001;
+
+				//Check if the LSB is set. If so, then the key was pressed since
+				//the last call to GetAsyncKeyState
+				bool unprocessedPress = ( ( keyState >> 0 )  & 0x0001 ) == 0x0001;
+
+				if( prntScrnIsPressed )
+				{
+					PlaySound();
+				}
+
+				if( unprocessedPress )
+				{
+					PlaySound();
+				}
+
+				Thread.Sleep( 100 );
+			}
 		}
 	}
 }
